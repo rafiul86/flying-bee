9e91f774f92157b9903ad09cd216a034acfa10ef
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2021-03-21 20:59:02 +0600

    submit button semi solved

--- src/Components/Destination/Destination.js
+++ src/Components/Destination/Destination.js
@@ -63,7 +63,7 @@ const Destination = () => {
     return (
                 <Grid container style={{display : 'flex'}}>
                     <Grid item  xs={12} lg={4}>
-            {userLocation.isVaid && <div> <img src="" alt=""/> <p style={{color : 'goldenrod' , fontWeight : '20px' }}>Thank you for choosing { userLocation.startLocation} the ride {userLocation.endLocation} . Your rider will contact you in seconds !</p> </div>}
+            {userLocation.isVaid && <div> <img src="" alt=""/> <p style={{color : 'goldenrod' , fontWeight : '20px' }}>Thank you for choosing from { userLocation.startLocation} to {userLocation.endLocation} . Your rider will contact you in seconds !</p> </div>}
           
                 <label for="start"><input type="text" onBlur={handleLocation} name="startLocation" id="start"  placeholder="your starting point" required/></label>
                 <br/>
